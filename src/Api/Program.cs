2f2649999b468802c5bac80886a9946854258aad
PabloMedinaGLA <pmedina@andreani.com>
2023-04-24 13:42:42 -0300

    configure Cors

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
     policy =>
     {
-        policy.WithOrigins("http://localhost:3000");
+        policy.WithOrigins(builder.Configuration["localhostAPI"]).AllowAnyMethod().AllowAnyHeader();
     });
 });
 
